75944f9bad5e553c19e1e626df1d861a54467f5f
Mostaza5330 <128562434+Mostaza5330@users.noreply.github.com>
2024-12-14 11:55:30 -0700

    identificación de capas

---
+++ Dominio_Reserva/src/main/java/com/mycompany/dominio_reserva/Dominio_Reserva.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dominio_reserva;
+
+/**
+ *
+ * @author Sebastian Murrieta Verduzco - 233463
+ */
+public class Dominio_Reserva {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Negocio_Reserva/src/main/java/com/mycompany/negocio_reserva/Negocio_Reserva.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.negocio_reserva;
+
+/**
+ *
+ * @author Sebastian Murrieta Verduzco - 233463
+ */
+public class Negocio_Reserva {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
